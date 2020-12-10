71cdaa9588ed65ad694259b8fa0de8742e3ec6be
lyrio94 <alexandrelyrio94@hotmail.com>
2020-12-10 19:17:59 -0300

    Commit referente tratamento de mensagens background

--- App.js
+++ App.js
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { Alert } from 'react-native';
+import messaging from '@react-native-firebase/messaging';
 
 import Home from './screen/Home'
 
 const App = () => {
+
+	useEffect(() => {
+
+		const unsubscribe = messaging().onMessage(async remoteMessage => {
+			Alert.alert('Nova mensagem chegou!', JSON.stringify(remoteMessage));
+		});
+
+		return unsubscribe;
+	}, []);
+
 	return <Home />
 }
 
