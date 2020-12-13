8dd7de480dcba6b43f6dc070e6163deca2b4b054
lyrio94 <alexandrelyrio94@hotmail.com>
2020-12-13 01:27:45 -0300

    correcao no app.js

--- App.js
+++ App.js
@@ -1,19 +1,20 @@
-import React, { useEffect, useState } from "react";
-import { AppRegistry } from 'react-native';
+import React, { useEffect, useState } from 'react'
+import { Alert } from 'react-native';
 import messaging from '@react-native-firebase/messaging';
-import "react-native-gesture-handler";
-import { createStackNavigator } from "@react-navigation/stack";
-import { NavigationContainer } from "@react-navigation/native";
-import Tabs from "./routes/Tabs";
-import Camera from "./screen/Camera";
-import ChooseIcon from "./screen/ChooseIcon";
-import ProfileToEdit from "./screen/ProfileToEdit";
-import { ProfileContext } from "./context/ProfileContext";
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { ProfileContext } from './context/ProfileContext'
 import { name as appName } from './app.json';
+import Tabs from './routes/Tabs'
+import ProfileToEdit from './screen/ProfileToEdit'
+import ChooseIcon from './screen/ChooseIcon'
+import Camera from './screen/Camera'
+import { configureLanguageToI18n } from './languages/utils'
 
 const Stack = createStackNavigator();
 
 const AppNetflix = () => {
+
   useEffect(() => {
     messaging().onNotificationOpenedApp(remoteMessage => {
       Alert.alert(
@@ -30,36 +31,30 @@ const AppNetflix = () => {
     return unsubscribe;
   }, []);
 
-  const [user, newUser] = useState("Jose");
-  console.log("user", user);
+  const [user, changeUser] = useState('Jose')
+  console.log('user', user);
+  configureLanguageToI18n()
+
   return (
-    <ProfileContext.Provider value={{ user, newUser }}>
+    <ProfileContext.Provider value={{ user, changeUser }}>
       <NavigationContainer>
         <Stack.Navigator>
-          <Stack.Screen
-            name="Tabs"
+          <Stack.Screen name="Tabs"
             component={Tabs}
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen
-            name="Camera"
+            options={{ headerShown: false }} />
+          <Stack.Screen name="ProfileToEdit"
+            component={ProfileToEdit} />
+          <Stack.Screen name="ChooseIcon"
+            component={ChooseIcon} />
+          <Stack.Screen name="Camera"
             component={Camera}
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen
-            name="ChooseIcon"
-            component={ChooseIcon}
-            options={{ headerShown: true }}
-          />
-          <Stack.Screen
-            name="ProfileToEdit"
-            component={ProfileToEdit}
-            options={{ headerShown: true }}
-          />
+            options={{ headerShown: false }} />
         </Stack.Navigator>
       </NavigationContainer>
     </ProfileContext.Provider>
-  );
-};
+  )
+}
 AppRegistry.registerComponent(appName, () => AppNetflix);
 export default AppNetflix;
+
+
